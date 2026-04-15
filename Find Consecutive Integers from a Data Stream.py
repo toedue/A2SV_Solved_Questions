9919533aaaeb89aad3139f006399142f26950655
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-04-15 14:37:13 +0300

    Add DataStream class for consecutive integer tracking
    Implement DataStream class to track consecutive integers.

---
+++ Find Consecutive Integers from a Data Stream.py
@@ -0,0 +1,25 @@
+class DataStream:
+
+    def __init__(self, value: int, k: int):
+        self.value = value
+        self.k = k
+        self.streak = 0
+        
+
+    def consec(self, num: int) -> bool:
+        if num == self.value:
+            self.streak += 1
+        else:
+            self.streak = 0
+
+        if self.streak >= self.k:
+            return True
+        else:
+            return False
+
+        
+
+
+# Your DataStream object will be instantiated and called as such:
+# obj = DataStream(value, k)
+# param_1 = obj.consec(num)
